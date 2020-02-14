ef0fb7d7ebe8dc7bd810f834919f15b2f8e115a4
RedCAT <akaineko18@gmail.com>
2020-02-14 18:20:46 +1300

    handling local data

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -12,6 +12,7 @@ const GET_MOVIES = gql`
     movies {
       id
       medium_cover_image
+      isLiked @client
     }
   }
 `;
@@ -62,7 +63,6 @@ const Movies = styled.div`
 
 const Home = () => {
   const { loading, data } = useQuery(GET_MOVIES);
-  console.log(data);
   return (
     <Container>
       <Header>
@@ -72,7 +72,12 @@ const Home = () => {
       {loading && <Loading>Loading...</Loading>}
       <Movies>
         {data?.movies?.map(m => (
-          <Movie key={m.id} id={m.id} bg={m.medium_cover_image} />
+          <Movie
+            key={m.id}
+            id={m.id}
+            isLiked={m.isLiked}
+            bg={m.medium_cover_image}
+          />
         ))}
       </Movies>
     </Container>
