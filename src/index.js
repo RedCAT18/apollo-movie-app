63121a61d611f5d07e3ea128748032b2fabe8c33
RedCAT <akaineko18@gmail.com>
2020-02-14 17:33:21 +1300

    data fetching w apollo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import client from './apollo';
+import { ApolloProvider } from '@apollo/react-hooks';
 
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
---
+++ src/routes/Detail.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import { gql } from 'apollo-boost';
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/react-hooks';
+import styled from 'styled-components';
+
+const GET_MOVIE = gql`
+  query getMovie($id: Int!) {
+    movie(id: $id) {
+      title
+      medium_cover_image
+      language
+      rating
+      description_intro
+    }
+    suggestions(id: $id) {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: white;
+`;
+
+const Column = styled.div`
+  margin-left: 10px;
+  width: 50%;
+`;
+
+const Title = styled.h1`
+  font-size: 48px;
+  margin-bottom: 15px;
+`;
+
+const Subtitle = styled.h4`
+  font-size: 28px;
+  font-weight: 200;
+  margin-bottom: 15px;
+`;
+
+const Description = styled.p`
+  font-size: 18px;
+  font-weight: 100;
+  line-height: 1.1;
+  text-indent: 20px;
+`;
+
+const Poster = styled.div`
+  width: 30%;
+  min-width: 300px;
+  height: 60%;
+  min-height: 480px;
+  background-color: transparent;
+  background-image: url(${props => props.bg});
+  background-size: cover;
+  background-position: center center;
+`;
+
+const Detail = () => {
+  const { id } = useParams();
+  const { loading, data } = useQuery(GET_MOVIE, {
+    variables: { id: parseInt(id) }
+  });
+  console.log(data);
+  return (
+    <Container>
+      <Column>
+        <Title>{loading ? 'Loading...' : data.movie.title}</Title>
+        <Subtitle>
+          {data?.movie?.language} | {data?.movie?.rating}
+        </Subtitle>
+        <Description>{data?.movie?.description_intro}</Description>
+      </Column>
+      <Poster bg={data?.movie?.medium_cover_image}></Poster>
+    </Container>
+  );
+};
+
+export default Detail;
